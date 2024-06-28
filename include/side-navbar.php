361cc7c0b3237248044e05af8acf10f2301256d2
TeerawatT <94170163+TeerawatT@users.noreply.github.com>
2024-06-28 22:20:50 +0700

    fix .htaccess & side-navbar

--- include/side-navbar.php
+++ include/side-navbar.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet" href="../assets/style.css">
     <!-- bootstrap-5 -->
     <link rel="stylesheet" href="../assets/bootstrap-5.3.3-dist/css/bootstrap.min.css">
-    <script src="../assets/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
     <link href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css" rel="stylesheet">
@@ -292,6 +291,8 @@
             </ul>
         </nav>
     </section>
+
+    <script src="../assets/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
 
 <!-- toggle each sub_nav_side -->
     <script>
