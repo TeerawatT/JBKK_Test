28ba2f2a66c44367569cac1b36dad5105b698392
TeerawatT <94170163+TeerawatT@users.noreply.github.com>
2024-06-28 17:29:25 +0700

    Add composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'teerawat/php-kbkk-teera',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '47d93484c72bae03eb8b24b26ab9dc0a1464ca00',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'teerawat/php-kbkk-teera' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '47d93484c72bae03eb8b24b26ab9dc0a1464ca00',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
